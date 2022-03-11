63a9ec4842c26c09288a8ea3f84e767a43ddbe98
Andy.Klenzman <92612316+andyKlenzman@users.noreply.github.com>
2022-03-10 19:04:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js'
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
 
 const canvas = document.querySelector(".webgl")
